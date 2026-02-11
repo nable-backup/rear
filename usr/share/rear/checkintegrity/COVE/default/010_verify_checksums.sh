fe016522fa6947ace883ea8c7f73ac5f769c3cf2
Andrus Suvalau <andrus.suvalau@n-able.com>
2026-02-11 12:43:50 +0100

    feat(cove): add more log messages
    Jira-Ref: BCF-6406: [REAR] Improve post-BMR integrity chec

---
+++ usr/share/rear/checkintegrity/COVE/default/010_verify_checksums.sh
@@ -0,0 +1,56 @@
+# Check md5sum files
+
+function verify_checksums() {
+    local md5sum_files=()
+
+    case "$COVE_INTEGRITY_CHECK" in
+        all)
+            md5sum_files+=(files.md5sum cove-files.md5sum)
+            ;;
+        *binaries*)
+            md5sum_files+=(cove-files.md5sum)
+            ;;
+        *configs*)
+            md5sum_files+=(files.md5sum)
+            ;;
+    esac
+
+    if [ ${#md5sum_files[@]} -eq 0 ]; then
+        LogUserOutput "Nothing to check. See COVE_INTEGRITY_CHECK in 'default.conf'."
+        return 0
+    fi
+
+    LogUserOutput "Checking if certain restored files are consistent with the recreated system..."
+
+    local all_pass=1
+    for md5sum_file in "${md5sum_files[@]}" ; do
+        local path="$VAR_DIR/layout/config/$md5sum_file"
+
+        # Skip when there are no checksums for this file
+        if ! test -s "$path"; then
+            LogUserOutput "Warning: '$path' not found. It will be skipped."
+            continue
+        fi
+
+        LogUserOutput "Verifying checksums from '$path'..."
+
+        local md5sum_stdout
+        if ! md5sum_stdout="$( md5sum -c --quiet < "$path" )" ; then
+            LogUserOutput "Restored file(s) do not fully match the recreated system."
+            LogUserOutput "$( sed -e 's/^/  /' <<< "$md5sum_stdout" )"
+            LogUserOutput "Verification failed: checksums do not match for file(s) in '$md5sum_file'."
+            all_pass=0
+        else
+            LogUserOutput "Verification passed: all checksums match in '$md5sum_file'."
+        fi
+    done
+
+    if [ $all_pass -eq 1 ]; then
+        LogUserOutput ""
+        LogUserOutput "Verification passed: all checksums match successfully."
+    else
+        Error "Verification failed: computed checksum did NOT match."
+    fi
+}
+
+verify_checksums
