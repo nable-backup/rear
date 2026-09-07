19bd88607e7dd485ca31ff6afbda1f14cc53e8c7
Andrus Suvalau <andrus.suvalau@n-able.com>
2026-09-07 10:56:37 +0200

    Change SELinux mode from enforcing to permissive on Fedora 44

--- usr/share/rear/lib/linux-functions.sh
+++ usr/share/rear/lib/linux-functions.sh
@@ -355,3 +355,22 @@ function get_lang () {
 
     return 1
 }
+
+# Changes the SELinux mode from enforcing to permissive and prints a warning about the change
+function set_selinux_permissive() {
+    local os=$1
+
+    selinux_config="$TARGET_FS_ROOT/etc/selinux/config"
+
+    [ -f "$selinux_config" ] || return 0
+
+    grep -q "^SELINUX=enforcing" "$selinux_config" || return 0
+
+    sed -i 's/^SELINUX=enforcing/SELINUX=permissive/' "$selinux_config"
+
+    local text="During the recovery process of ${os}-based systems, SELinux is set to permissive mode to \
+successfully relabel the entire file system. After verifying that your system is functioning \
+correctly, set SELINUX=enforcing in /etc/selinux/config to return SELinux to enforcing mode."
+
+    cove_print_in_frame "WARNING" "$text" || return 0
+}
